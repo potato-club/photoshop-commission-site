e98dcd3b2e32677576da50fb2adbc7d0a6f761ae
SongJae Lim <thdwo999@naver.com>
2022-04-09 18:59:28 +0900

    header, footer 컴포넌트 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import type { NextPage } from 'next';
-import Header from '../src/components/header';
-import Footer from '../src/components/footer';
 
 const Home: NextPage = () => {
-  return <div>
-    <Header></Header>
-    <Footer></Footer>
-  </div>;
+  return <div>Index Page</div>;
 };
 
 export default Home;
