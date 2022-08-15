7a21b4ff5b8c7cfdb0e5ffac57acd9a0421d2855
UNNEW-main <1970005826c@gmail.com>
2022-08-15 15:13:05 +0900

    [ADD] 마이페이지 후기남기기 퍼블리싱

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Pagination, { ReactJsPaginationProps } from 'react-js-pagination';
 
+type props = {
+  activePage: number;
+  onChange: (pageNumber: number) => void;
+  totalItemsCount: number;
+  perPage?: number;
+};
 export const CustomPagination = ({
   activePage,
   onChange,
   totalItemsCount,
-}: ReactJsPaginationProps) => {
+  perPage = 15,
+}: props) => {
   return (
     <Pagination
       activePage={activePage}
-      itemsCountPerPage={15}
+      itemsCountPerPage={perPage}
       totalItemsCount={totalItemsCount}
       onChange={onChange}
       pageRangeDisplayed={5}
--- src/containers/mypage/components/MyPageLayout.tsx
+++ src/containers/mypage/components/MyPageLayout.tsx
@@ -14,6 +14,10 @@ const NavItemInfo = [
     pathName: '/mypage/rate',
   },
   {
+    name: '후기 남기기',
+    pathName: '/mypage/review',
+  },
+  {
     name: '작성 글',
     pathName: '/mypage/post/all',
   },
