21166f11c0c64952d4c47aafbe0edd62c312e67a
thdwo999@naver.com <thdwo999@naver.com>
2022-08-20 17:54:42 +0900

    fix. CustomPagination

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import Pagination, { ReactJsPaginationProps } from 'react-js-pagination';
 
-type props = {
-  activePage: number;
-  onChange: (pageNumber: number) => void;
-  totalItemsCount: number;
-  perPage?: number;
-};
 export const CustomPagination = ({
   activePage,
   onChange,
   totalItemsCount,
-  perPage = 15,
-}: props) => {
+  itemsCountPerPage,
+}: ReactJsPaginationProps) => {
   return (
     <Pagination
       activePage={activePage}
-      itemsCountPerPage={perPage}
+      itemsCountPerPage={itemsCountPerPage}
       totalItemsCount={totalItemsCount}
       onChange={onChange}
       pageRangeDisplayed={5}
