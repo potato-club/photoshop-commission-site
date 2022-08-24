bab859abe62f3560f1babc1b8c4621f381a1b61b
thdwo999@naver.com <thdwo999@naver.com>
2022-08-24 17:47:29 +0900

    feat. animation 적용

--- src/containers/IntroPage/IntroPage.tsx
+++ src/containers/IntroPage/IntroPage.tsx
@@ -1,21 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Typography } from 'src/components';
 import { customColor } from 'src/constants';
+import Link from 'next/link';
 
 const IntroPage = () => {
+  const HeaderImgFrame = keyframes`
+  0%{
+    position: relative;
+    top:100px;
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+    position:relative;
+    top:0px;
+  }
+  `;
+  const HeaderTextFrame = keyframes`
+    0%{
+        position: relative;
+        right: 1000px;
+    }
+    100%{
+        position:relative;
+        right: 0px;
+    }
+  `;
+  const SectionImgFrame = keyframes`
+    0%{
+        position: relative;
+        top: 350px;
+        opacity: 0;
+    }
+    100%{
+        position:relative;
+        top: 0px;
+        opacity: 1;
+    }
+  `;
+  const SectionTextFrame = keyframes`
+  0%{
+      position: relative;
+      left: 1000px;
+  }
+  100%{
+      position:relative;
+      left: 0px;
+  }
+`;
+  const FooterImgFrame = keyframes`
+    0%{
+        position: relative;
+        top: 180px;
+        right: 120px;
+        opacity: 0;
+    }
+    100%{
+        position: relative;
+        top: 0px;
+        right: 0px;
+        opacity: 1;
+    }
+  `;
+  const FooterTextFrame = keyframes`
+    0%{
+        opacity: 0;
+    }
+    100%{
+        opacity: 1;
+    }
+    `;
   return (
     <Container>
-      <HeaderImageBox>
+      <HeaderImageBox frame={HeaderImgFrame}>
         <Image
-          src="/image 92.png"
+          src="/image92.png"
           width="1180px"
           height="640px"
           alt="이미지가 없습니다.."
         />
       </HeaderImageBox>
-      <HeaderTextBox>
+      <HeaderTextBox frame={HeaderTextFrame}>
         <Typography size="48" fontWeight="900">
           소중한 사진들을 편집하는데
         </Typography>
@@ -24,10 +91,10 @@ const IntroPage = () => {
         </Typography>
       </HeaderTextBox>
 
-      <SectionImageBox>
+      <SectionImageBox frame={SectionImgFrame}>
         <ImageBox>
           <Image
-            src="/image 43.png"
+            src="/image43.png"
             width="480px"
             height="640px"
             alt="이미지가 없습니다.."
@@ -38,7 +105,7 @@ const IntroPage = () => {
         </ImageBox>
         <ImageBox>
           <Image
-            src="/image 44.png"
+            src="/image44.png"
             width="480px"
             height="640px"
             alt="이미지가 없습니다.."
@@ -48,7 +115,7 @@ const IntroPage = () => {
           </Typography>
         </ImageBox>
       </SectionImageBox>
-      <SectionTextBox>
+      <SectionTextBox frame={SectionTextFrame}>
         <Typography size="60" fontWeight="900">
           또는, 고객의 요구사항대로
         </Typography>
@@ -56,14 +123,16 @@ const IntroPage = () => {
           편집하는 역량을 기르고 싶다면?
         </Typography>
       </SectionTextBox>
-      <Image
-        src="/image 93.png"
-        width="1180px"
-        height="760px"
-        alt="이미지가 없습니다.."
-      />
+      <FooterImageBox frame={FooterImgFrame}>
+        <Image
+          src="/image93.png"
+          width="1180px"
+          height="760px"
+          alt="이미지가 없습니다.."
+        />
+      </FooterImageBox>
 
-      <FooterTextBox>
+      <FooterTextBox frame={FooterTextFrame}>
         <Typography size="60" fontWeight="900">
           이곳에서 시도해보세요
         </Typography>
@@ -74,16 +143,24 @@ const IntroPage = () => {
         </Typography>
       </FooterTextBox>
       <BtnBox>
-        <LoginBtn>
-          <Typography size="16" fontWeight="900" color="lightBlue">
-            로그인 및 회원가입
-          </Typography>
-        </LoginBtn>
-        <GoToBtn>
-          <Typography size="16" fontWeight="900" color="white">
-            게시글 보러가기
-          </Typography>
-        </GoToBtn>
+        <Link href="/main">
+          <a>
+            <LoginBtn>
+              <Typography size="16" fontWeight="900" color="lightBlue">
+                로그인 및 회원가입
+              </Typography>
+            </LoginBtn>
+          </a>
+        </Link>
+        <Link href="/main">
+          <a>
+            <GoToBtn>
+              <Typography size="16" fontWeight="900" color="white">
+                게시글 보러가기
+              </Typography>
+            </GoToBtn>
+          </a>
+        </Link>
       </BtnBox>
     </Container>
   );
@@ -121,27 +198,39 @@ const BtnBox = styled.div`
 const TextBox = styled.div`
   text-align: center;
 `;
-const FooterTextBox = styled(TextBox)`
+const FooterImageBox = styled.div<{ frame: any }>`
+  animation: ${prop => prop.frame} 3s linear alternate;
+  width: 100%;
+  text-align: center;
+`;
+const FooterTextBox = styled(TextBox)<{ frame: any }>`
   margin: 250px 0;
+  animation: ${prop => prop.frame} 10s;
 `;
-const SectionTextBox = styled(TextBox)`
+const SectionTextBox = styled(TextBox)<{ frame: any }>`
   margin: 0 0 250px 0;
+  animation: ${prop => prop.frame} 5s;
 `;
-const SectionImageBox = styled.div`
+const SectionImageBox = styled.div<{ frame: any }>`
   width: 100%;
   display: flex;
   justify-content: space-around;
   margin-bottom: 250px;
+  animation: ${prop => prop.frame} 3s linear alternate;
 `;
 const ImageBox = styled.div``;
-const HeaderTextBox = styled(TextBox)`
+const HeaderTextBox = styled(TextBox)<{ frame: any }>`
   margin: 25px 0 250px 0;
+  animation: ${prop => prop.frame} 5s;
 `;
-const HeaderImageBox = styled.div`
+const HeaderImageBox = styled.div<{ frame: any }>`
   margin: 40px 0 50px 0;
+  animation: ${prop => prop.frame} 3s linear alternate;
 `;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100vw;
+  overflow-y: hidden;
 `;
