8a88a26706c0d980e15185121940bbd1ff55a060
thdwo999@naver.com <thdwo999@naver.com>
2022-09-22 22:05:21 +0900

    feat. 로그인 버튼 기능 구현중

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,31 @@
 import Link from 'next/link';
 import { Typography } from '../components/index';
 import { HeaderName } from 'src/constants/header/HeaderName';
-import styled from 'styled-components';
-import { FaUserAlt } from 'react-icons/fa';
-import { FaBell } from 'react-icons/fa';
+import styled, { css } from 'styled-components';
+import { FaUserAlt, FaBell } from 'react-icons/fa';
+import { BiLogIn, BiLogOut } from 'react-icons/bi';
 import { pathName } from 'src/constants/pathName';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
+  const router = useRouter();
+  const callKaKaoLoginHandler = () => {
+    router.push({
+      pathname: process.env.NEXT_PUBLIC_PATHNAME,
+      query: {
+        response_type: process.env.NEXT_PUBLIC_RESPONSE_TYPE,
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI,
+      },
+    });
+  };
+  const checkSession = () => {
+    if (typeof window !== 'undefined') {
+      console.log(localStorage.getItem('session'));
+      return localStorage.getItem('session');
+    }
+  };
+
   return (
     <HeaderBox>
       <ContentBox>
@@ -18,6 +37,10 @@ export const Header = () => {
           </Theme>
         </Link>
         <Icons>
+          <div onClick={() => callKaKaoLoginHandler()}>
+            {checkSession() ? <LogoutImage /> : <LoginImage />}
+          </div>
+
           <Link href={pathName.MYPAGE.PROFILE}>
             <a>
               <UserImage />
@@ -36,6 +59,20 @@ export const Header = () => {
 };
 
 export default Header;
+
+const iconStyles = css`
+  width: 20px;
+  height: 20px;
+  color: white;
+  padding: 8px 8px;
+  border: 4px solid white;
+  border-radius: 100%;
+  :hover {
+    color: gray;
+    border: 4px solid gray;
+  }
+  margin-right: 5px;
+`;
 
 export const HeaderBox = styled.div`
   background-color: rgba(7, 104, 159, 1);
@@ -63,28 +100,15 @@ const Icons = styled.div`
   justify-content: space-between;
 `;
 
-const UserImage = styled(FaUserAlt)`
-  width: 20px;
-  height: 20px;
-  color: white;
-  padding: 8px 8px;
-  border: 4px solid white;
-  border-radius: 100%;
-  :hover {
-    color: gray;
-    border: 4px solid gray;
-  }
+const LogoutImage = styled(BiLogOut)`
+  ${iconStyles}
 `;
-
+const LoginImage = styled(BiLogIn)`
+  ${iconStyles}
+`;
+const UserImage = styled(FaUserAlt)`
+  ${iconStyles}
+`;
 const AlertImage = styled(FaBell)`
-  width: 20px;
-  height: 20px;
-  color: white;
-  padding: 8px 8px;
-  border: 4px solid white;
-  border-radius: 100%;
-  :hover {
-    border: 4px solid gray;
-    color: gray;
-  }
+  ${iconStyles}
 `;
