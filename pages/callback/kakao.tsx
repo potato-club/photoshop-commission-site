fa01ad4240937dc6ca95049c8106845ae92752e4
KoJaem <woalswhwh@naver.com>
2022-10-13 20:50:36 +0900

    feat : 백엔드api 확인을 위한 commit

--- pages/callback/kakao.tsx
+++ pages/callback/kakao.tsx
@@ -11,31 +11,38 @@ export default function CheckToken() {
   const { setStorage } = useLocalStorage();
 
   const checkUser = useCallback(async () => {
-    const { data } = await signUpApi.checkUser({
+    const allData = await signUpApi.checkUser({
       params: {
         code,
       },
     });
 
-    console.log(data);
+    console.log(allData);
+    console.log('======================');
+    console.log('이 아래가 헤더임. 여기에 토큰들값이 들어가있으면 됨');
+    console.log(allData.headers);
+    console.log('======================');
+
 
     // 회원가입을 제대로 하지 않고 중간에 나갔을때 : fail
-    if (data.fail) {
-      router.push('/main')
-      alert("이전에 회원가입이 제대로 진행되지 않았습니다. 다시 로그인 해주세요.");
+    if (allData.data.fail) {
+      router.push('/main');
+      alert(
+        '이전에 회원가입이 제대로 진행되지 않았습니다. 다시 로그인 해주세요.',
+      );
     }
 
     // 받는거(최초 로그인일때) : 이메일, 메세지
-    if (data.SerialCode) {
-      setStorage('serialCode', data.SerialCode);
+    if (allData.data.SerialCode) {
+      setStorage('serialCode', allData.data.SerialCode);
       router.push('/signUp');
       return;
     }
 
     // 최초 로그인이 아닐때 : 세션, 메세지
-    if (data.session) {
+    if (allData.data.session) {
       console.log('세션!');
-      setStorage('session', data.session);
+      setStorage('session', allData.data.session);
       router.push('/main');
       return;
     }
