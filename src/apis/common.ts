241eafb9a6631924c2da6f36dad61d45d16c5473
KoJaem <woalswhwh@naver.com>
2022-11-25 01:27:46 +0900

    feat : api 추가

--- src/apis/common.ts
+++ src/apis/common.ts
@@ -9,14 +9,41 @@ export default {
   getWithParams: async (url: string, params: any) =>
     await axios.get(setting.baseUrl + url, params),
 
-  post: async (url: string, params: any) =>
-    await axios.post(setting.baseUrl + url, params),
-
-  postWithToken: async (url: string, params: any, accessToken: any, refreshToken: any) =>
-    await axios.post(setting.baseUrl + url, params, {
+  getWithToken: async (url: string, accessToken: any, refreshToken: any) =>
+    await axios.get(setting.baseUrl + url, {
       headers: {
-        authorization : accessToken,
-        refreshToken
-      }
-    })
+        authorization: accessToken,
+        refreshToken,
+      },
+    }),
+
+  getWithParamsToken: async (
+    url: string,
+    params: any,
+    accessToken: any,
+    refreshToken: any,
+  ) =>
+    await axios.get(setting.baseUrl + url, {
+      params,
+      headers: {
+        authorization: accessToken,
+        refreshToken,
+      },
+    }),
+
+  post: async (url: string, data: any) =>
+    await axios.post(setting.baseUrl + url, data),
+
+  postWithToken: async (
+    url: string,
+    data: any,
+    accessToken: any,
+    refreshToken: any,
+  ) =>
+    await axios.post(setting.baseUrl + url, data, {
+      headers: {
+        authorization: accessToken,
+        refreshToken,
+      },
+    }),
 };
