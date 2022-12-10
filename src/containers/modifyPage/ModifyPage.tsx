026d300ae7cf44fe591a578739af96ecf5c4b862
KoJaem <woalswhwh@naver.com>
2022-12-11 03:43:02 +0900

    feat : 글 수정하기 api 연동 #59

--- src/containers/modifyPage/ModifyPage.tsx
+++ src/containers/modifyPage/ModifyPage.tsx
@@ -3,38 +3,53 @@ import styled from 'styled-components';
 import { customColor } from 'src/constants';
 import { FieldValues, useForm } from 'react-hook-form';
 import { Header, Contents, Buttons } from './components';
-import { BoardType } from 'src/types/board.type';
 import { ImageType } from 'src/types/image.type';
+import { boardApi } from 'src/apis/board';
+import { useRouter } from 'next/router';
+import { useGetToken } from 'src/hooks/useGetToken';
+
 type Props = {
   title: string;
   imageUrls: ImageType[];
   contents: string;
 };
 export const ModifyPage = ({ title, imageUrls, contents }: Props) => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const { query: { id } } = useRouter();
+  const { access, refresh } = useGetToken();
   const submit = async (data: FieldValues) => {
-    // 로직
-    console.log(data);
+    const { title, contents, imageOpen } = data;
+    const frm = new FormData();
+    frm.append('title', title);
+    frm.append('context', contents);
+    frm.append('imageOpen', imageOpen);
+    try {
+      const res = await boardApi.update(id, frm, access, refresh);
+      console.log(res);
+    } catch(e) {
+      console.log(e);
+    }
   };
-
-  // boardNo: number;
-  // title: string;
-  // state: string;
-  // writer: string;
-  // createdDate: string;
-  // modifiedDate: string;
-  // imageUrls: ImageType[];
-  // imageOpen: imageOpenType;
-  // contents: string;
-  // totalComment: number;
-  // commentList: CommentType[];
-
   return (
     <Container>
       <Wrapper>
         <form onSubmit={handleSubmit(submit)}>
-          <Header register={register} title={title}/>
-          <Contents register={register} imageUrls={imageUrls} contents={contents}/>
+          <Header
+            register={register}
+            control={control}
+            errors={errors}
+            title={title}
+          />
+          <Contents
+            imageUrls={imageUrls}
+            register={register}
+            contents={contents}
+          />
           <Line />
           <Buttons />
         </form>
@@ -62,4 +77,9 @@ const Wrapper = styled.div`
 const Line = styled.div`
   margin-bottom: 20px;
   border-bottom: 1px solid ${customColor.gray};
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  position: relative;
 `;
