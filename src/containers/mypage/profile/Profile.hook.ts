1464430c7837b11dc90d976cdd69fe8ccc5b79a5
UNNEW-main <1970005826c@gmail.com>
2022-12-17 14:43:30 +0900

    [ADD] 프로필 자기소개 예외처리

--- src/containers/mypage/profile/Profile.hook.ts
+++ src/containers/mypage/profile/Profile.hook.ts
@@ -1,4 +1,5 @@
 import { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import { errorModal } from 'src/utils/interactionModal';
 
 export const useProfile = () => {
   const [nickname, setNickname] = useState('');
@@ -16,10 +17,10 @@ export const useProfile = () => {
     try {
       if (isInfoChange) {
         if (nickname.length > 8) {
-          alert('닉네임은 8글자 이하로 설정해주세요');
+          errorModal('닉네임은 8글자 이하로 설정해주세요');
           return;
         } else if (nickname.length === 0) {
-          alert('닉네임을 입력해주세요.');
+          errorModal('닉네임을 입력해주세요.');
           return;
         }
         //Todo: API에 저장하기
@@ -37,6 +38,7 @@ export const useProfile = () => {
       if (isIntroduceChange) {
         if (introduce === '') {
           //Todo: 자기소개 공백안된다고 경고모달 들어가야함
+          errorModal('자기소개를 작성해주세요');
           return;
         }
         //Todo: API에 저장하기
