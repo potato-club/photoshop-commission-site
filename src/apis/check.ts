db51994cd56eb824c039bc4aba4c9883283f3343
KoJaem <woalswhwh@naver.com>
2022-12-19 01:10:29 +0900

    modify : api 리팩토링 #38

--- src/apis/check.ts
+++ src/apis/check.ts
@@ -2,8 +2,11 @@ import api from './common';
 
 export const checkApi = {
   checkJob: async () =>
-    api.getWithParamsToken('check/enum', undefined),
+    api.getWithAuth('check/enum'),
 
   checkWriter: async (id:any) =>
-    api.getWithParamsToken('check/writer', id)
+    api.getWithAuth('check/writer', id),
+
+  checkSelectedArtist: async (id:any) =>
+    api.get(`check/${id}/artist`)
 };
--- src/apis/signUpApi.ts
+++ src/apis/signUpApi.ts
@@ -3,7 +3,7 @@ import api from './common';
 export const signUpApi = {
   signUp: async (data: any) => api.post('signup', data),
 
-  checkUser: async (data: any) => api.getWithParams('check/user', data),
+  checkUser: async (data: any) => api.get('check/user', data),
 
-  checkNickname: async (data: any) => api.getWithParams('signup/checkbox', data),
+  checkNickname: async (data: any) => api.get('signup/checkbox', data),
 };
