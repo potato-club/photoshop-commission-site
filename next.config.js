965e63d4656379e1c2edbcccaf76df74e30f2fb4
KoJaem <woalswhwh@naver.com>
2022-12-25 03:08:49 +0900

    yml 수정

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
-  env: {
-    NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
-    NEXT_PUBLIC_PATHNAME: process.env.NEXT_PUBLIC_PATHNAME,
-    NEXT_PUBLIC_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
-    NEXT_PUBLIC_RESPONSE_TYPE: process.env.NEXT_PUBLIC_RESPONSE_TYPE,
-  },
 };
 
 module.exports = nextConfig;
