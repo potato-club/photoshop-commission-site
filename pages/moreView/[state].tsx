2fe1fc7fda09110e851c8eecfcdad69a5ddcad53
KoJaem <woalswhwh@naver.com>
2023-01-24 22:49:13 +0900

    fix : MoreViewPage 로직 수정 #91

--- pages/moreView/[state].tsx
+++ pages/moreView/[state].tsx
@@ -1,11 +1,23 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
 import MoreViewPage from 'src/containers/MoreViewPage/MoreViewPage';
+import { useMoreViewData } from 'src/hooks/useMoreViewData';
 
 const State: NextPage = () => {
+  const [page, setPage] = useState(1);
+  const { data, theme, total } = useMoreViewData(page);
+  const isData = data && theme && total;
   return (
     <>
-      <MoreViewPage />
+      {isData && (
+        <MoreViewPage
+          data={data}
+          theme={theme}
+          total={total}
+          page={page}
+          setPage={setPage}
+        />
+      )}
     </>
   );
 };
